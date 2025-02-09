83b395d6d35b341f6f9308dff440fa68a6eff3f0
Shougi <116136870+sosgisi@users.noreply.github.com>
2025-02-09 19:08:52 +0700

    config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <title>BHM</title>
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
+    @vite(['resources/js/app.jsx'], secure: true)
     @inertiaHead
     @routes
 </head>
