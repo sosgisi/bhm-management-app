6f93f030cbccfdf7ce5ee58a9c5e639a8d8faf8b
Shougi <116136870+sosgisi@users.noreply.github.com>
2025-02-15 17:39:16 +0700

    update pagination

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -4,14 +4,21 @@ const Pagination = ({ pagination }) => {
     return (
         <nav className="flex space-x-2 justify-center my-5">
             {pagination.map((link, index) => (
-                <Link
-                    key={index}
-                    href={link.url || ''}
-                    className={`flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600
-                        ${link.active ? 'bg-gray-300' : ''} 
-                        ${!link.url ? '!text-gray-500' : ''}`}
-                    dangerouslySetInnerHTML={{ __html: link.label }}
-                />
+                link.url ? (
+                    <Link
+                        key={index}
+                        href={link.url}
+                        dangerouslySetInnerHTML={{ __html: link.label }}
+                        className={`flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600
+                            ${link.active ? 'bg-gray-300' : ''}`}
+                    />
+                ) : (
+                    <span
+                        key={index}
+                        dangerouslySetInnerHTML={{ __html: link.label }}
+                        className="text-slate-300"
+                    ></span>
+                )
             ))}
         </nav>
     )
