4ed651a6c3e66308af3a71a8955bb469093afe39
Shougi <116136870+sosgisi@users.noreply.github.com>
2025-02-16 10:32:35 +0700

    handle https request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->app['request']->server->set('HTTPS', true);
         URL::forceScheme('https');
         Inertia::share([
             'auth' => function (Request $request) {
